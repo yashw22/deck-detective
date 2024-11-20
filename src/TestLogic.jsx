ef345874a324e5de438070d6bf7ce8161843beae
Yash Wardhan <45715307+yashw22@users.noreply.github.com>
2024-11-19 19:27:03 -0500

    update to Card testing

--- src/TestLogic.jsx
+++ src/TestLogic.jsx
@@ -1,15 +1,31 @@
 import { getSearchDeck, getWeaponDeck } from "./lib/utils";
 
+import PropertyCard from "./components/card/PropertyCard";
+import InvestigationSheet from "./components/InvestigationSheet/InvestigationSheet";
+
 export default function TestLogic() {
   const weaponDeck = getWeaponDeck();
-  console.log("weaponDeck: ", weaponDeck);
+  // // console.log("weaponDeck: ", weaponDeck);
 
-  const searchDeck = getSearchDeck();
-  console.log("searchDeck: ", searchDeck);
+  // const searchDeck = getSearchDeck();
+  // console.log("searchDeck: ", searchDeck);
+
+  var cards = [];
+  for (let i = 0; i < 36; i++) {
+    cards.push(
+      <PropertyCard
+        color={weaponDeck[i].color}
+        type={weaponDeck[i].weapon}
+        number={weaponDeck[i].type}
+      />
+    );
+  }
 
   return (
     <div>
       <h1>Test Logics</h1>
+      <InvestigationSheet />
+      <div className="test-container">{cards}</div>
     </div>
   );
 }
